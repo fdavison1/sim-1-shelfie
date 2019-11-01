79115eb1cade5eb2afa2af5f3d6eb34cfb189d7d
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-11-01 16:40:56 -0600

    css 2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,15 @@ export default function Header(){
     return(
         <header>
             
+           
+           <header className='logo2'>
             <div className="logo">V</div>
            <h1>SHELFIE</h1>
+           </header>
             
-
+            <img
+            src='https://pbs.twimg.com/profile_images/378800000729097494/240cc17eb32a0b014683ed5a99dab86f.jpeg'
+            />
            
         </header>
     )
