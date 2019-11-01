b3cd010883694c8d02ebc4c1e515eecdd9693514
Fred Davison <fredbook@Freds-MacBook-Pro.local>
2019-11-01 09:23:04 -0600

    component tree

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export default class Form extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+    render(){
+        return(
+            <div>Form.jsx</div>
+        )
+    }
+}
